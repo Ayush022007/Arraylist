67fddcc6438a08bc318a1ee582396ac6b36b9343
Ayush022007 <singhayush89076@gmail.com>
2026-09-18 04:52:51 +0530

    new

--- que1.java
+++ que1.java
@@ -1,15 +1,22 @@
 package Arraylist;
 import java.util.ArrayList; 
-public class que1 {
-    public static void String(String Args[]){
+
+public class que1{
+    public static void main(String Args[]){
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<String> list2 = new ArrayList<>();
-        ArrayList<Boolean> list3 = new ArrayList<>();
+        // ArrayList<String> list2 = new ArrayList<>();
+        // ArrayList<Boolean> list3 = new ArrayList<>();
 
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
-        System.out.println(list);
+        list.add(5);
+        // System.out.println(list);
+        // System.out.println(list.get(2));
+        // System.out.println(list.remove(2));
+        // System.out.println(list);
+        list.set(3,355);
+        System.err.println(list);
     }
 }
