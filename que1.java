76ab500755c37886a82195228aa1ce6b8646861c
Ayush022007 <singhayush89076@gmail.com>
2026-09-17 21:23:19 +0530

    new

--- que1.java
+++ que1.java
@@ -1,5 +1,15 @@
 package Arraylist;
-
+import java.util.ArrayList; 
 public class que1 {
-    
+    public static void String(String Args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
 }
